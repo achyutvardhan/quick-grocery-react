d6e0160566773f28c6f5ce5a4ab7151b07a789ff
gaurav <gnahata@deloitte.com>
2025-06-20 10:41:42 +0530

    home page

--- quick-grocery/src/pages/Home.jsx
+++ quick-grocery/src/pages/Home.jsx
@@ -2,13 +2,15 @@ import React , {useContext, useEffect} from 'react'
 import { CartContext } from '../context/CartContext'
 import HeroBanner from '../Home/HeroBanner'
 import TopCategory from '../Home/TopCategory'
+import { AuthContext } from '../context/AuthContext'
 // import OurProductsSection from  '../Home/OurProductSection'
 // import FoodBlogSection from '../Home/FoodBlogSection'
 
 export default function Home() {
   const {refreshFetch} = useContext(CartContext);
+  const {loggedIn} = useContext(AuthContext)
   useEffect(()=>{
-    refreshFetch();
+   {loggedIn && refreshFetch()}
   },[])
   return (
     <>
